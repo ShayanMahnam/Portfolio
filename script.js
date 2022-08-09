40deaa880c5dceb8fa1d9c84c216dea29e27ac55
Shayan Mahnam <95313895+Shayanma94@users.noreply.github.com>
2022-08-09 14:51:11 +0100

    transition wire updated

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ var typed = new Typed(".input", {
 });
 
 var chngClr = document.getElementById("chng-clr");
-var darkText = document.getElementById("dark-text")
+var darkText = document.getElementById("dark-text");
+var wire = document.getElementById("wire");
 const audio = new Audio();
 audio.src = "./sounds/switch.mp3";
 
 chngClr.onclick = function(){
     document.body.classList.toggle("dark-theme");
     audio.play();
+    wire.style.height = "130px";
     if (document.body.classList.contains("dark-theme")) {
         darkText.textContent = "on";
     } else {
         darkText.textContent = "off";
+        wire.style.height = "120px";
     }
 }
