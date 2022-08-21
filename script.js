33ed8bed24e72ef2f1f985ef68f362e0bcdf1c2a
Shayan Mahnam <95313895+Shayanma94@users.noreply.github.com>
2022-08-21 23:15:46 +0100

    popup box added

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+//typing
 var typed = new Typed(".input", {
     strings:['Shayan Mahnam.', 'Full Stack Developer.'],
     typeSpeed: 70,
     backSpeed: 60,
     loop: true
 });
-
+//night mode
 var chngClr = document.getElementById("chng-clr");
 var darkText = document.getElementById("dark-text");
 var wire = document.getElementById("wire");
@@ -28,17 +29,47 @@ chngClr.onclick = function(){
     }
 }
 
+//button portfolio
 const menu = document.querySelector('#toggle');  
-const menuItems = document.querySelector('#overlay');  
-const menuContainer = document.querySelector('.menu-container');  
-const menuIcon = document.querySelector('i');  
+const menuItems = document.querySelector('#overlay');   
 
-function toggleMenu(e) {
+function toggleMenu() {
     menuItems.classList.toggle('open');
-    menuContainer.classList.toggle('full-menu');
-    menuIcon.classList.toggle('fa-bars');
-    menuIcon.classList.add('fa-times');
-    e.preventDefault();
+}
+menu.addEventListener('click', toggleMenu, false);
+
+
+
+//advert slide
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-menu.addEventListener('click', toggleMenu, false);
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
