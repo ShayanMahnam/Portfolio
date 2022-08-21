ab09b863b8f49f0ce9f665631936073def46c613
Shayan Mahnam <95313895+Shayanma94@users.noreply.github.com>
2022-08-21 15:31:54 +0100

    add popup

--- script.js
+++ script.js
@@ -27,3 +27,18 @@ chngClr.onclick = function(){
         
     }
 }
+
+const menu = document.querySelector('#toggle');  
+const menuItems = document.querySelector('#overlay');  
+const menuContainer = document.querySelector('.menu-container');  
+const menuIcon = document.querySelector('i');  
+
+function toggleMenu(e) {
+    menuItems.classList.toggle('open');
+    menuContainer.classList.toggle('full-menu');
+    menuIcon.classList.toggle('fa-bars');
+    menuIcon.classList.add('fa-times');
+    e.preventDefault();
+}
+
+menu.addEventListener('click', toggleMenu, false);
